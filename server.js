1f38f80a6c4c32d41b5654a534f5cc80ec844a50
tovestas <110106681+tovestas@users.noreply.github.com>
2022-07-27 18:03:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({message: 'olá mundo estou funcionando'});
 })
 
+app.get('/olá', (req, res) => {
+    res.status(200).send({message: 'olá mundo});
+})
+
 app.listen(3001, () => {
     console.log('api rodando na porta 3001');
 }) 
